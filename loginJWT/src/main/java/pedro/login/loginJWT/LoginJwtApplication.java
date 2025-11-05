276bfaf8782835fb973656dfff810e4a24917123
Pedro Henrique Costa Santiago <pedro.santiago8051@outlook.com>
2025-11-05 12:01:48 -0300

    First commit

---
+++ loginJWT/src/main/java/pedro/login/loginJWT/LoginJwtApplication.java
@@ -0,0 +1,13 @@
+package pedro.login.loginJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginJwtApplication.class, args);
+	}
+
+}
